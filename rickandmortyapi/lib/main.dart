426085e8d55cb5c500e291546b33540acf0d06c5
S7EIN07 <steinjailson@gmail.com>
2025-09-09 15:02:14 -0300

    FEAT: Tekla inicial e é isso

---
+++ rickandmortyapi/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:rickandmortyapi/ui/pages/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen()));
+}
