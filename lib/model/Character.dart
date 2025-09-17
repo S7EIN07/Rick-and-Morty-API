709cf0b7bae2b35bd83c7e9953508ff69015b316
S7EIN07 <steinjailson@gmail.com>
2025-09-17 14:59:40 -0300

    FEAT: model response

--- lib/model/Character.dart
+++ lib/model/Character.dart
@@ -55,3 +55,18 @@ class Character {
     );
   }
 }
+
+class CharacterResponse {
+  Info info;
+  List<Character> results;
+  CharacterResponse({required this.info, required this.results});
+
+  factory CharacterResponse.fromJson(Map<String, dynamic> json) {
+    return CharacterResponse(
+      info: Info.fromJson(json['info']),
+      results: List<Character>.from(
+        json['results'].map((x) => Character.fromJson(x)),
+      ),
+    );
+  }
+}
