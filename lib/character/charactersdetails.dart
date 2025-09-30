fe41708e41fe93bcdbaf1daba368ef324fd738c8
S7EIN07 <steinjailson@gmail.com>
2025-09-30 14:47:23 -0300

    FEAT: Lista de personagens

---
+++ lib/character/charactersdetails.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CharactersDetailsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Character Details")),
+      body: Center(child: Text("Character Details Here")),
+    );
+  }
+}
--- lib/character/characterslist.dart
+++ lib/character/characterslist.dart
@@ -1,19 +1,22 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
+import 'package:rickandmortyapi/character/charactersdetails.dart';
+import 'package:rickandmortyapi/model/Character.dart';
 
 class CharactersPage extends StatefulWidget {
   @override
   State<CharactersPage> createState() => _CharactersPageState();
 }
 
-
 class _CharactersPageState extends State<CharactersPage> {
-  Future<http.Response> getCharacters() async {
-    final response = await http.get(Uri.parse('https://rickandmortyapi.com/api/character'));
+  Future<List<Character>> getCharacters() async {
+    final response = await http.get(
+      Uri.parse('https://rickandmortyapi.com/api/character'),
+    );
     if (response.statusCode == 200) {
-      // criar objeto
-      return response;
+      return CharacterResponse.fromJson(json.decode(response.body)).results;
     } else {
       throw Exception('Failed to load characters');
     }
@@ -23,10 +26,41 @@ class _CharactersPageState extends State<CharactersPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Characters")),
-      body: FutureBuilder<http.Response>(
+      body: FutureBuilder(
         future: getCharacters(),
         builder: (context, snapshot) {
-          return Center(child: Text("CHARACTERS"));
+          if (!snapshot.hasData) {
+            return Center(child: CircularProgressIndicator());
+          } else {
+            // mostrar na tela
+            var characters = snapshot.data as List<Character>;
+            return ListView.builder(
+              itemCount: characters.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Image.network(characters[index].image),
+                  title: Text(characters[index].name),
+                  subtitle: Text(characters[index].species),
+                  trailing: MouseRegion(
+                    cursor: SystemMouseCursors.click, // Adicione esta linha
+                    child: GestureDetector(
+                      child: Container(
+                        child: Text("Detalhes", style: TextStyle(fontSize: 16)),
+                      ),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => CharactersDetailsPage(),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                );
+              },
+            );
+          }
         },
       ),
     );
